e7f982c027d438843f1721307d866b956e2015ab
Greg Odintsov <Mynamenoname1996@gmail.com>
2021-04-04 19:42:39 +0300

    Initial Commit

---
+++ PersonalQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by Gregory Odintsov on 04.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
