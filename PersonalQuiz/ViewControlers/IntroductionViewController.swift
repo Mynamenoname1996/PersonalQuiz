4b95e62de1c35edf1378a5bd749936e8b72fd06b
Greg Odintsov <Mynamenoname1996@gmail.com>
2021-04-10 15:51:06 +0300

    Done

---
+++ PersonalQuiz/ViewControlers/IntroductionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  IntroductionViewController.swift
+//  PersonalQuiz
+//
+//  Created by Gregory Odintsov on 04.04.2021.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    @IBAction func unwind(segue: UIStoryboardSegue) {}
+
+
+}
+
